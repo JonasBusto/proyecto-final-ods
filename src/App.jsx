baf141aacaeabc5e7aa6ef2836623d1fa65d8e25
JonasBusto <jonasrex00@gmail.com>
2023-11-12 13:53:36 -0300

    bug rutas

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,15 @@ import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import { BrowserRouter } from "react-router-dom";
 import "./styles/app.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="d-flex flex-column min-vh-100">
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <div className="d-flex flex-column min-vh-100">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
   );
 };
 
